6cdb4133bd13994b53a039c06d03cb87067bf782
Og'abek542 <123234816+ogabek542@users.noreply.github.com>
2025-09-11 19:19:06 +0500

    change main image

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -22,13 +22,14 @@ const Photo = () => {
           }}
           className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
+          {/* image of portfolio owner  */}
           <Image
-            src="/assets/manprofile.png"
+            src="/assets/nextPortfolio.png"
             priority
             quality={100}
             fill
             alt="profile image"
-            className="object-contain"
+            className="object-contain rounded-full"
           />
         </motion.div>
         {/* circle */}
@@ -46,15 +47,19 @@ const Photo = () => {
             strokeWidth="4"
             strokeLinecap="round"
             strokeLinejoin="round"
-            initial={{strokeDasharray:"24 10 0 0 "}}
+            initial={{ strokeDasharray: "24 10 0 0 " }}
             animate={{
-                strokeDasharray:["15 120 25 25 ","16 25 92 72"," 4 250 22 22 "],
-                rotate:[120,360],
+              strokeDasharray: [
+                "15 120 25 25 ",
+                "16 25 92 72",
+                " 4 250 22 22 ",
+              ],
+              rotate: [120, 360],
             }}
             transition={{
-                duration:20,
-                repeat:Infinity,
-                repeatType:"reverse",
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "reverse",
             }}
           />
         </motion.svg>
